708515a2b6ceb56623ab2911d17286982878eb30
phTononFerreira <pehetofe@gmail.com>
2023-03-14 23:11:51 -0300

    🔗 Mudança da baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export function setupApiClient(){
 
     const api = axios.create({
-        baseURL: 'https://angie-portfolio-api.onrender.com',
+        baseURL: 'angie-portfolio-backend-production.up.railway.app',
         headers: {
             secretcode: process.env.REACT_APP_SECRET_CODE
         }

8d1c8daa59092f518904c2f2d910bb232d026020
phTononFerreira <pehetofe@gmail.com>
2023-07-08 16:58:35 -0300

    🔗 Inserção links

--- src/pages/Contato/index.jsx
+++ src/pages/Contato/index.jsx
@@ -14,28 +14,28 @@ export default function Contato() {
             <div className="mainDivContato">
                 <div className='gridContato'>
                     <div className='contatoDivP'>
-                        <a className='contatoDiv instagram'>
+                        <a target="_blank" href='https://www.instagram.com/_sanchez.angela_/' className='contatoDiv instagram'>
                             <h2>Instagram</h2>
                             <BsInstagram size={40} />
                         </a>
                     </div>
 
                     <div className='contatoDivP'>
-                        <a className='contatoDiv linkedin'>
+                        <a target="_blank" href='https://www.linkedin.com/in/angelasanchezalves/' className='contatoDiv linkedin'>
                             <h2>LinkedIn</h2>
                             <BsLinkedin size={40} />
                         </a>
                     </div>
 
                     <div className='contatoDivP'>
-                        <a className='contatoDiv whatsapp'>
+                        <a target="_blank" href='https://wa.me/5511989000918' className='contatoDiv whatsapp'>
                             <h2>Whatsapp</h2>
                             <BsWhatsapp size={40} />
                         </a>
                     </div>
 
                     <div className='contatoDivP'>
-                        <a className='contatoDiv email'>
+                        <a target="_blank" href='mailto:sanchez.18a18@gmail.com' className='contatoDiv email'>
                             <h2>Email</h2>
                             <HiOutlineMail size={40} />
                         </a>

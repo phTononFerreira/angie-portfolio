5233981a8a690409f5a3d779a34a423aab3551e1
phTononFerreira <pehetofe@gmail.com>
2023-08-01 16:24:10 -0300

    🔗 Alteração API

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export function setupApiClient(){
 
     const api = axios.create({
-        baseURL: 'https://angie-portfolio-backend-production.up.railway.app',
+        baseURL: 'angie-portfolio-backend.vercel.app',
         headers: {
             secretcode: process.env.REACT_APP_SECRET_CODE
         }
